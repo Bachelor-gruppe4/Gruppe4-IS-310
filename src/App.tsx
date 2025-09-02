37ca7c87106305020ce99f6a09ba9c4bdb16dcee
Anna Østmoen <annaostmoen@Anna-sin-Air.uia.no>
2025-09-02 12:53:56 +0200

    la til om oss boks

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,23 @@ function App() {
         Gruppe 4
       </div>
       <div style={{ marginTop: '4.5rem' }}>
+        <div style={{
+          maxWidth: '1100px',
+          margin: '0 auto 2rem auto',
+          textAlign: 'center',
+          background: '#efefefff',
+          padding: '1.5rem',
+          borderRadius: '10px',
+          color: '#333'
+        }}>
+          <h2>Om oss</h2>
+          <p>
+            Vi er en gruppe på 5 studenter som studerer Bachelor IT og informasjonssystemer på UiA. Gruppen vår har samarbeidet gjennom hele studiet og har lært hverandres styrker og svakheter, samt hvordan man best håndterer konflikter innad i gruppen. Vi jobber strukturert og samarbeider godt for å oppnå gode resultater.
+          </p>
+          <p>
+            Vi har en god blanding av forskjellige ferdigheter, og dermed ønsker vi å jobbe med noe innenfor prosjektutvikling, som vi har erfaring med fra diverse prosjekter gjennom UiA i samarbeid med blant annet Kartverket og Digin, praksis hos Moment Analyse og arbeidsoppdrag fra Daltex Resindekke.
+          </p>
+        </div>
         <h1>Team Profiles</h1>
         <div style={{ display: 'flex', flexDirection: 'column', gap: '1.5rem', alignItems: 'center' }}>
           {profiles.map((profile, idx) => (
